2cbc42f6bc172e473122c8e8ca26467d51c62ab7
Khoo Yong Jun <programmer.sk95@gmail.com>
2021-10-26 18:33:31 +0800

    added tests to cover the functionality

---
+++ tests/populate-shouldNotPopulate.test.js
@@ -0,0 +1,42 @@
+const populator = require('../');
+
+const populateData = {
+  domain_1: {
+    field_1: 'value 1',
+  }
+}
+
+let {
+  populate,
+  massPopulate,
+  populateFactory
+} = populator(false, populateData);
+
+test('it will return initial_value', () => {
+  const initial_value = 'initial_value';
+
+  const outcome = populate('domain_1', 'field_1', initial_value);
+
+  expect(outcome).toBe(initial_value);
+});
+
+test('it will return populate fields value', () => {
+  const initial_value = 'initial_value';
+
+  const outcome = massPopulate('domain_1', {
+    field_1: initial_value
+  });
+
+  expect(outcome).toEqual({
+    field_1: initial_value
+  });
+});
+
+test('it will return populate function of certain domain', () => {
+  const initial_value = 'initial_value';
+
+  const populate = populateFactory('domain_1');
+  const outcome = populate('field_1', initial_value);
+
+  expect(outcome).toBe(initial_value);
+});
---
+++ tests/populate-shouldPopulate.test.js
@@ -0,0 +1,59 @@
+const populator = require('../');
+
+const populateData = {
+  domain_1: {
+    field_1: 'value 1',
+  }
+}
+
+let {
+  populate,
+  massPopulate,
+  populateFactory
+} = populator(true, populateData);
+
+test('it will return populate value', () => {
+  const initial_value = 'initial_value';
+
+  const outcome = populate('domain_1', 'field_1', initial_value);
+
+  expect(outcome).toBe(populateData['domain_1']['field_1']);
+});
+
+test('it will return populate fields value', () => {
+  const initial_value = 'initial_value';
+
+  const outcome = massPopulate('domain_1', {
+    field_1: initial_value
+  });
+
+  expect(outcome).toEqual(populateData['domain_1']);
+});
+
+test('it will return populate function of certain domain', () => {
+  const initial_value = 'initial_value';
+
+  const populate = populateFactory('domain_1');
+  const outcome = populate('field_1', initial_value);
+
+  expect(outcome).toBe(populateData['domain_1']['field_1']);
+});
+
+test('it will throw error on empty initial value', () => {
+  const test = () => {
+    populate('domain_1', 'field_1');
+  }
+
+  expect(test).toThrow(Error);
+  expect(test).toThrow('Initial Value is invalid');
+});
+
+test('it will throw error on populate value and initial value mismatch', () => {
+  const initial_value = 0;
+  const test = () => {
+    populate('domain_1', 'field_1', initial_value);
+  }
+
+  expect(test).toThrow(TypeError);
+  expect(test).toThrow(`domain_1.field_1 is ${populateData['domain_1']['field_1'].constructor.name}, and initial value is ${initial_value.constructor.name}`);
+});
